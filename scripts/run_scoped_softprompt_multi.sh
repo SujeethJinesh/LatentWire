085a45a99400740d0c54b0bee8594b85ab03ca60
SujeethJinesh <sujeethjinesh@gmail.com>
2025-09-20 20:35:03 -0700

    tune(stage-b): disable KD losses to save memory

--- scripts/run_scoped_softprompt_multi.sh
+++ scripts/run_scoped_softprompt_multi.sh
@@ -142,6 +142,7 @@ CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES}" python -u latentwire/train.py \
   --save_dir "$CKPT_DIR" --resume_from "$CKPT_DIR" --no_load_optimizer --save_training_stats \
   --train_append_bos_after_prefix yes \
   --first_token_ce_weight 2.0 \
+  --k_ce_weight 0.0 --kd_first_k_weight 0.0 --state_kd_weight 0.0 \
   --adapter_hidden_mult 2 \
   --manifold_stat_weight 0.001 \
   --max_answer_tokens 24 \
