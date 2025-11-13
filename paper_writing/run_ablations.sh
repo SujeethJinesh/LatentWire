219b60d6f989f1661d1ba5a5fbdd6dea9d305f66
SujeethJinesh <sujeethjinesh@gmail.com>
2025-11-12 21:55:31 -0800

    chore: raise eval batch size to 64

--- paper_writing/run_ablations.sh
+++ paper_writing/run_ablations.sh
@@ -71,7 +71,7 @@ run_experiment() {
             --per_device_batch "$PER_DEVICE_BATCH" \
             --eval_every "$EVAL_EVERY" \
             --eval_samples "$EVAL_SAMPLES" \
-            --eval_batch_size 16 \
+            --eval_batch_size 64 \
             --max_new_tokens "$MAX_NEW_TOKENS" \
             --bf16 \
             --no_compile \
